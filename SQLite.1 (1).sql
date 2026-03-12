4f82d6f9fb1e7ca134181ea7944ece07932cb4f7
LucasdosSantdo67 <j.moura04@escola.pr.gov.br>
2026-03-12 16:50:48 -0300

    Add files via upload

---
+++ SQLite.1 (1).sql
@@ -0,0 +1,9 @@
+INSERT INTO Hortifruit (descricao,valor,comentario)
+VALUES
+('morango',6.70,'morango do amor'),
+('Laranja',8.90,'laranja lima'),
+('Uva',2.90,'uva roxa'),
+('Maça',2.90,'maça do amor'),
+('Pera',5.80,'pera verde amarelada'),
+('Maça Verde',6.90,'maça do amor verde'),
+('Banana',8.90,'uva roxa');
